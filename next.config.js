0bb939811e407bb8f6dd527fa915490d410010a5
dancielos <iam@dancielos.com>
2023-12-10 20:37:44 -0700

    added some dev and prod envirnoment on next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+// const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 
-module.exports = nextConfig
+const nextConfig = {
+	output: 'export',
+	// styledComponents: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+	},
+};
+
+module.exports = (phase, { defaultConfig }) => {
+	if (phase === PHASE_PRODUCTION_BUILD) {
+		return { ...nextConfig, basePath: '/wp-content/linkinbio' };
+	}
+	return nextConfig;
+};
